33a41b88b79434d6e2618d56e102182f61029777
taehoon95 <leeth950304@gmail.com>
2021-05-28 12:39:16 +0900

    web 연결

--- src/main/java/spring5_rest_study/config/MvcConfig.java
+++ src/main/java/spring5_rest_study/config/MvcConfig.java
@@ -1,14 +1,25 @@
 package spring5_rest_study.config;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 @Configuration
 @EnableWebMvc
@@ -38,6 +49,17 @@ public class MvcConfig implements WebMvcConfigurer{
         return ms;
     }
   
-
+	// 날짜 설정 용
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		ObjectMapper objectMapper = Jackson2ObjectMapperBuilder
+				.json()
+				.featuresToEnable(SerializationFeature.INDENT_OUTPUT)
+				.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(formatter))
+				.simpleDateFormat("yyyy-MM-dd HH:mm:ss")
+				.build();
+		converters.add(0, new MappingJackson2HttpMessageConverter(objectMapper));
+	}
 
 }
